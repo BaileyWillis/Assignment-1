9981400b9cea495be1b8c54ef6c0b5cbc764967d
Bailey Willis <3279258@uon.edu.au>
2019-04-03 08:31:37 +0000

    Added comments

--- Assignment1.c
+++ Assignment1.c
@@ -7,7 +7,6 @@
 int main()
 {
     
-    //push this
     char cipher_method;
  
     printf("Please select one of the following options: \n");
@@ -18,32 +17,32 @@ int main()
     printf("(E) Decryption of a message encrypted with a rotation cipher given cipher text only \n");
     printf("(F) Decryption of a message encrypted with a substitution cipher given cipher text only \n\n");
     
-    scanf("%c", &cipher_method);
+    scanf("%c", &cipher_method);                     //scans the option that the user would like to run
     
     
     
     
-    switch(cipher_method)
+    switch(cipher_method)                            //whatever option was chosen from the main menu, it will perform the appropriate case
     {
-        case 'A':
+        case 'A':                                    //run option "A" from the main menu
         {
 	char encryption[100];
 	char character;
 	int i;
 	int key;
 	
-	printf("Enter a message to encrypt: \n"); //user input for their sentence to be encrypted
-	scanf("%s",encryption); //scanning the message entered by the user and placing it in the string 'encryption'
-	printf("Enter key: "); //A Key is how many places the encryption rotates the characters eg. key of 3 will turn 'A' into 'D'
-	scanf("%d", &key); //scanning the key entered by the user for the code to execute 
+  	printf("Enter a message to encrypt: \n");        //user input for their sentence to be encrypted
+	scanf("%s",encryption);                          //scanning the message entered by the user and placing it in the string 'encryption'
+	printf("Enter key: ");                           //A Key is how many places the encryption rotates the characters eg. key of 3 will turn 'A' into 'D'
+ 	scanf("%d", &key);                               //scanning the key entered by the user for the code to execute 
 	
-	for(i = 0; encryption[i] != '\0'; ++i)
-	{
-		character = encryption[i];
+	for(i = 0; encryption[i] != '\0'; ++i)           //A for loop that: starts by asigning the string 'encryption' to zero,                
+	{                                                //string 'encryption' is not equal to NULL and the string is incriminated by 1  
+		character = encryption[i];                   //Asigns 'encryption' string to 'character'
 		
-		if(character >= 'a' && character <= 'z')
+		if(character >= 'a' && character <= 'z')     //If statement with conditions (if character is greater than or equal to "a") AND (if character is less than or equal to "z")
 		{
-			character = character + key;
+			character = character + key;             //
 			
 			if(character > 'z')
 			{
